7ee868e0f0873e1de4636b6927e242b6e84b5fd5
RSMarian <marianstefan1499@gmail.com>
2022-12-16 14:03:25 +0200

    cached dom

---
+++ app.js
@@ -0,0 +1,13 @@
+const userScore = 0;
+const compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+rock_div.addEventListener('click', function() {
+    console.log("poop");
+})
