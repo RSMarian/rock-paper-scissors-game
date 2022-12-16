1f55330cd65b3dfad946c50a2ffd31b2e4619bb1
RSMarian <marianstefan1499@gmail.com>
2022-12-16 14:35:40 +0200

    added computer-side functionality
    random choice between rock, paper, scissors

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const userScore = 0;
 const compScore = 0;
 const userScore_span = document.getElementById("user-score");
-const compScore_span = document.getElementById("comp-score");
+const compScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-rock_div.addEventListener('click', function() {
-    console.log("poop");
-})
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+console.log(getComputerChoice());
+
+
+function game(userChoice) {
+
+}
+
+function main(){
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+}
+
+main();
