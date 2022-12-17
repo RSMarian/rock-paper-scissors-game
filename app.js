da4b63b80bc92ee963fe65968dce367612d16c12
RSMarian <marianstefan1499@gmail.com>
2022-12-17 14:37:42 +0200

    added functionality
    'computer'-side functionality, glow when win/lose/draw, timeout to that glow

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const compScore_span = document.getElementById("computer-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -14,25 +14,76 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-console.log(getComputerChoice());
 
-
-function game(userChoice) {
-
-}
 
 function main(){
-    rock_div.addEventListener('click', function() {
-        game("rock");
-    })
-
-    paper_div.addEventListener('click', function() {
-        game("paper");
-    })
-
-    scissors_div.addEventListener('click', function() {
-        game("scissors");
-    })
+    rock_div.addEventListener('click', () => game("rock"));
+    paper_div.addEventListener('click', () =>game("paper"));
+    scissors_div.addEventListener('click', () =>game("scissors"));
 }
 
 main();
+
+
+function capitalize(word) {
+    if (word === "rock") return "Rock";
+    if (word === "paper") return "Paper";
+    if (word === "scissors") return "Scissors";
+}
+
+
+function win(userChoice, computerChoice){
+    const smallUserWord = "(user)".fontsize(3).sup();
+    const smallCompWord = "(comp)".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${capitalize(userChoice)}${smallUserWord} beats ${capitalize(computerChoice)}${smallCompWord}. You win!`;
+    userChoice_div.classList.add('green-glow');
+    setTimeout(() => userChoice_div.classList.remove('green-glow'), 500);
+}
+
+function lose(userChoice, computerChoice){
+    const smallUserWord = "(user)".fontsize(3).sup();
+    const smallCompWord = "(comp)".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${capitalize(userChoice)}${smallUserWord} loses to ${capitalize(computerChoice)}${smallCompWord}. You lose...`;
+    userChoice_div.classList.add('red-glow');
+    setTimeout(() => userChoice_div.classList.remove('red-glow'), 500);
+}
+
+function draw(userChoice, computerChoice){
+    const smallUserWord = "(user)".fontsize(3).sup();
+    const smallCompWord = "(comp)".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${capitalize(userChoice)}${smallUserWord} equals ${capitalize(computerChoice)}${smallCompWord}. It's a draw.`;
+    userChoice_div.classList.add('gray-glow');
+    setTimeout(() => userChoice_div.classList.remove('gray-glow'), 500);
+}
+
+
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            win(userChoice, computerChoice);
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            lose(userChoice, computerChoice);
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
